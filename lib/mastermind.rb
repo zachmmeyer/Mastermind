633e160332bf6658672e19e5144b3309489eef7f
Zach Meyer <zachary.michael.meyer@gmail.com>
2021-10-04 15:13:58 -0400

    Add rudimentary method to choose role in game

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -3,10 +3,24 @@
 module Mastermind
   # Game class
   class Game
+    def player_choose_role
+      player_choice = gets.chomp
+      case player_choice
+      when '1'
+        puts 'Player is The Codebreaker'
+      when '2'
+        puts 'Player is The Codemaker'
+      else
+        start
+      end
+    end
+
     def start
       system('clear')
       puts 'Welcome to Mastermind!'
       puts 'Press 1 to become The Codebreaker; Press 2 to become The Codemaker'
+      player_choose_role
+    end
   end
 end
 
