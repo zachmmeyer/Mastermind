d5fc334124260516aa677697156fb59bb7316a34
Zach Meyer <zachary.michael.meyer@gmail.com>
2021-10-06 16:31:10 -0400

    Move files to archive and start fresh

---
+++ too_much_work/utilities.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# Module containing various utilities
+module Utilities
+  def match_input_to_symbol(input, output_literal, output_symbols)
+    return unless output_literal.include?(input)
+
+    output_literal.each_with_index do |item, index|
+      if item == input
+        puts output_symbols[index]
+        break
+      end
+    end
+  end
+end
