b34a55574391e84a32b16a2348d43f8ebea2c9d1
Zach Meyer <zachary.michael.meyer@gmail.com>
2021-10-11 22:12:10 -0400

    Add methods and initialize codebreaker

--- lib/codebreaker.rb
+++ lib/codebreaker.rb
@@ -2,8 +2,26 @@
 
 # Codebreaker class
 class Codebreaker
-  def guess
+  def initialize
+    @guess_input = ''
+  end
+
+  def reset_guess
+    @guess_input = ''
+  end
+
+  def return_guess
+    @guess_input
+  end
+
+  def clear_and_prompt
+    system('clear')
+    puts 'Player is The Codebreaker'
+    puts 'Please input a 4-digit number'
+  end
+
+  def input_guess
     puts "\nInput your guess"
-    gets.chomp
+    @guess_input = gets.chomp
   end
 end
