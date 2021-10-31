c8db7ae0842e44eb5fca48fef3fcb9a6fde452b4
Zach Meyer <zachary.michael.meyer@gmail.com>
2021-10-31 13:05:02 -0400

    Fix codemaker feedback to be 100% accurate

--- lib/codemakercomputer.rb
+++ lib/codemakercomputer.rb
@@ -7,19 +7,27 @@ class CodemakerComputer
   def initialize
     @code = ''
     create_code
-    # @code = '1122'
   end
 
   def fill_in_spaces(code, feedback)
     code.length - feedback.length
   end
 
-  def inexact_match(filtered_code, guess)
-    # TODO: Filter out guess of inexact matches
-    # guess.chars.map.with_index do |char, index|
+  def filter_exact_matches(filtered_code, filtered_code_hash)
+    filtered_code.each do |char|
+      filtered_code_hash[char] += 1 unless char == 'exact match'
+    end
+  end
+
+  def inexact_match(filtered_code, filtered_guess)
+    filtered_code_hash = Hash.new(0)
+    filter_exact_matches(filtered_code, filtered_code_hash)
     count = 0
     filtered_code.each do |char|
-      count += 1 if guess.include?(char)
+      if filtered_guess.include?(char) && filtered_code_hash[char].positive?
+        count += 1
+        filtered_code_hash[char] -= 1
+      end
     end
     count
   end
@@ -37,10 +45,10 @@ class CodemakerComputer
   def return_feedback(guess)
     feedback = ''
     filtered_code = remove_exact_matches(@code, guess)
+    filtered_guess = remove_exact_matches(guess, @code)
     (filtered_code.count 'exact match').times { feedback += '+' }
-    inexact_match(filtered_code, guess).times { feedback += '-' }
+    inexact_match(filtered_code, filtered_guess).times { feedback += '-' }
     fill_in_spaces(@code, feedback).times { feedback += '_' }
-    puts feedback
     feedback
   end
 
@@ -50,8 +58,3 @@ class CodemakerComputer
     @code = rand(1111..6666).to_s until @code.match?(/^[1-6]{4}$/)
   end
 end
-
-# codemaker = CodemakerComputer.new
-
-# codemaker.return_feedback('1122')
-# codemaker.return_feedback('2211')
