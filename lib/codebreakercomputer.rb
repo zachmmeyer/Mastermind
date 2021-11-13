98566eb4d255e7958f454069eb7e0083662848f0
Zach Meyer <zachary.michael.meyer@gmail.com>
2021-11-12 19:58:11 -0500

    Remove broken computer guess algorithm

--- lib/codebreakercomputer.rb
+++ lib/codebreakercomputer.rb
@@ -15,6 +15,7 @@ class CodebreakerComputer < Codebreaker
   # Store all guesses
   # Make new guesses based on feedback received and stored guesses
 
+
   def input_guess
     # p @possible_codes
     # gets.chomp
@@ -23,38 +24,6 @@ class CodebreakerComputer < Codebreaker
     # puts "\nInput your guess"
     # @guess = gets.chomp
 
-    # This mess below will get to being eventually having all the right numbers in the wrong place
 
-    @feedback_total = @feedback_inexact + @feedback_exact
-    @guess = if @turn.zero?
-               @guess_array.join
-             elsif @feedback_total.zero? && @turn.positive?
-               (0..3).each do |number|
-                 @guess_array[number] += 1
-               end
-               @guess_array.join
-             elsif @feedback_total == 1 && @turn.positive?
-               (0..2).each do |number|
-                 @guess_array[number] += 1
-               end
-               @guess_array.join
-             elsif @feedback_total == 2 && @turn.positive?
-               (0..1).each do |number|
-                 @guess_array[number] += 1
-               end
-               @guess_array.join
-              elsif @feedback_total == 3 && @turn.positive?
-                (0..0).each do |number|
-                  @guess_array[number] += 1
-                end
-                @guess_array.join
-              elsif @feedback_total == 4 && @turn.positive?
-                @guess_array.shuffle!
-                while @previous_guesses.include?(@guess_array.join)
-                  @guess_array.shuffle!
-                end
-                @previous_guesses.push(@guess_array.join)
-                @guess_array.join
-             end
   end
 end
